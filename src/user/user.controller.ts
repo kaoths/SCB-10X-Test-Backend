360b669eb1a72e7fba62c75b6238df04f0b93f83
Tanawit <tanawit139@gmail.com>
2020-08-18 03:25:44 +0700

    add AuthGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserId } from '../decorators/user-id.decorator';
 import { User } from './user.model';
+import { AuthGuard } from '../guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly service: UserService) {}
-  @Get('me')
-  me(@UserId() id: string): Promise<User> {
+
+  @Get('self')
+  self(@UserId() id: string): Promise<User> {
     return this.service.findById(id);
   }
 }
